fc8028b811351c1ad9d56e37529044ffd11e951e
Aman Kumar <kumaraman21@gmail.com>
2011-11-23 02:50:07 -0600

    Aman - We can now specify a story runner main class in JBehave project settings.

---
+++ src/com/github/kumaraman21/intellijbehave/JBehaveConfigurable.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.kumaraman21.intellijbehave;
+
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class JBehaveConfigurable implements SearchableConfigurable {
+  private JBehaveSettingsPanel jBehaveSettingsPanel;
+
+  @NotNull
+  @Override
+  public String getId() {
+    return getHelpTopic();
+  }
+
+  @Override
+  public Runnable enableSearch(String s) {
+    return null;
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return "JBehave";
+  }
+
+  @Override
+  public Icon getIcon() {
+    return null;
+  }
+
+  @Override
+  public String getHelpTopic() {
+    return "reference.settingsdialog.project.jbehave";
+  }
+
+  @Override
+  public JComponent createComponent() {
+    if (jBehaveSettingsPanel == null) {
+      jBehaveSettingsPanel = new JBehaveSettingsPanel();
+    }
+    reset();
+    return jBehaveSettingsPanel.getContentPane();
+  }
+
+  @Override
+  public boolean isModified() {
+    if(jBehaveSettingsPanel != null) {
+      return jBehaveSettingsPanel.isModified();
+    }
+    return false;
+  }
+
+  @Override
+  public void apply() throws ConfigurationException {
+    if(jBehaveSettingsPanel != null) {
+      jBehaveSettingsPanel.apply();
+    }
+  }
+
+  @Override
+  public void reset() {
+    if(jBehaveSettingsPanel != null) {
+      jBehaveSettingsPanel.reset();
+    }
+  }
+
+  @Override
+  public void disposeUIResources() {
+    jBehaveSettingsPanel = null;
+  }
+}
---
+++ src/com/github/kumaraman21/intellijbehave/JBehaveSettings.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2000-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.kumaraman21.intellijbehave;
+
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+
+@State(
+  name = "JBehaveSettings",
+  storages = {@Storage(
+    id = "main",
+    file = "$APP_CONFIG$/jbehave_settings.xml"
+  )}
+)
+public class JBehaveSettings implements PersistentStateComponent<JBehaveSettings> {
+  private String storyRunner;
+
+  public static JBehaveSettings getInstance(){
+    return ServiceManager.getService(JBehaveSettings.class);
+  }
+
+  @Override
+  public JBehaveSettings getState() {
+    return this;
+  }
+
+  @Override
+  public void loadState(JBehaveSettings jBehaveSettings) {
+    XmlSerializerUtil.copyBean(jBehaveSettings, this);
+  }
+
+  public String getStoryRunner() {
+    return storyRunner;
+  }
+
+  public void setStoryRunner(String storyRunner) {
+    this.storyRunner = storyRunner;
+  }
+}
