87667c538846ea74a4443c649faf9f1b384344f2
Aman Kumar <kumaraman21@gmail.com>
2011-11-25 18:46:51 -0600

    Aman - Small correction in the spellings of a package.

--- src/com/github/kumaraman21/intellijbehave/creator/CreateStoryAction.java
+++ src/com/github/kumaraman21/intellijbehave/creator/CreateStoryAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.kumaraman21.intellijbehave.creater;
+package com.github.kumaraman21.intellijbehave.creator;
 
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.actions.CreateElementActionBase;
