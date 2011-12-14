072ab78d88375c1a2af2c5cb1122c9c5bcfd686d
Aman Kumar <kumaraman21@gmail.com>
2011-12-13 21:44:17 -0600

    Aman - Plugin now highlights an error on step text if step definition was not found.

---
+++ src/com/github/kumaraman21/intellijbehave/resolver/StoryAnnotator.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011 Aman Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.kumaraman21.intellijbehave.resolver;
+
+import com.github.kumaraman21.intellijbehave.highlighter.StoryTokenType;
+import com.github.kumaraman21.intellijbehave.parser.StepPsiElement;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import org.jetbrains.annotations.NotNull;
+
+public class StoryAnnotator implements Annotator {
+  @Override
+  public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder) {
+    if(! (psiElement instanceof LeafPsiElement)) {
+      return;
+    }
+    if(! (((LeafPsiElement)psiElement).getElementType() == StoryTokenType.STEP_TEXT)) {
+      return;
+    }
+
+    PsiElement parentElement = psiElement.getParent();
+    if(! (parentElement instanceof StepPsiElement)) {
+      return;
+    }
+
+    StepPsiElement parentStepElement = (StepPsiElement)parentElement;
+    if(parentStepElement.getReference().resolve() == null) {
+      annotationHolder.createErrorAnnotation(psiElement, "No definition found for step");
+    }
+  }
+}
